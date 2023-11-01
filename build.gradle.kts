e721b677ca4c0e8fa0dba9dce66415aa99ceb1ff
ian3314273 <ian3314273@gmail.com>
2023-11-01 15:01:23 +0800

    Initial commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,14 @@
 plugins {
     id("java")
+    application
 }
 
 group = "com.github.ian3314273"
 version = "1.0-SNAPSHOT"
+
+application {
+    mainClass = "com.github.ian3314273.Main"
+}
 
 repositories {
     mavenCentral()
@@ -13,6 +18,18 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
+tasks {
+    javadoc {
+        options.encoding = "UTF-8"
+    }
+    compileJava {
+        options.encoding = "UTF-8"
+    }
+    compileTestJava {
+        options.encoding = "UTF-8"
+    }
+}
+
 
 tasks.test {
     useJUnitPlatform()
